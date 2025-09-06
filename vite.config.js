b0a24756f6d0bae49c31c9d7bb6f11e0ccb81b06
Christopher <chris.axel.wood@gmail.com>
2025-09-06 22:31:42 +0200

    Update GitHub Pages configuration for Chriswood12/dnd-damage-calculator

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ElvenCrit/',  // Replace with your GitHub repository name
+  base: '/dnd-damage-calculator/',  // Your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
